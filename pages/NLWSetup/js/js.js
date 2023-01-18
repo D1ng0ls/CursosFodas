80b297bee395f8844a68b4b1ef8b63baadc64308
d1ng0ls <4b4c4t3666@gmail.com>
2023-01-18 12:42:54 -0300

    cursosfodas

--- pages/NLWSetup/js/js.js
+++
@@ -1,7 +0,0 @@
-function hidden_ul() {
-    if (document.getElementById('hidden').checked) {
-        document.getElementById('text').style.display = 'block';
-    } else {
-        document.getElementById('text').style.display = 'none';
-    }
-}
